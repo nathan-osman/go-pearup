cf671d6ccddbea96180abb15d97b2be254ad663e
Nathan Osman <nathan@quickmediasolutions.com>
2018-09-08 17:17:11 -0700

    Add database models.

---
+++ db/pearup.go
@@ -0,0 +1,17 @@
+package db
+
+import (
+	"time"
+)
+
+// Pearup represents a single pearup.
+type Pearup struct {
+	ID         int64
+	Name       string    `gorm:"not null"`
+	EndDate    time.Time `gorm:"not null"`
+	IsComplete bool      `gorm:"not null"`
+}
+
+func (p *Pearup) String() string {
+	return p.Name
+}
---
+++ db/user.go
@@ -0,0 +1,18 @@
+package db
+
+// User represents an authenticated Facebook user.
+type User struct {
+	ID              int64
+	FacebookID      string `gorm:"not null;unique_index"`
+	FacebookName    string `gorm:"not null"`
+	FacebookPicture string `gorm:"not null"`
+
+	// Profile data
+	Link     string `gorm:"not null"`
+	IsMale   bool   `gorm:"not null"`
+	IsFemale bool   `gorm:"not null"`
+}
+
+func (u *User) String() string {
+	return u.FacebookName
+}
